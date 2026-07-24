badaf9803d4cf7c0f7eb5ce0c4a9ade9ec0f76a5
KTejaReddy <tejareddykatta1@gmail.com>
2026-07-24 08:05:57 +0530

    fix(frontend): render logo using original asset instead of custom SVG

--- frontend/src/components/ui/AppIcon.tsx
+++ frontend/src/components/ui/AppIcon.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils'
+import arkoreLogo from '@/assets/branding/arkore-logo-transparent.png'
 
 interface AppIconProps {
   className?: string
@@ -17,15 +18,14 @@ export function AppIcon({ className, iconClassName }: AppIconProps) {
         className
       )}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-        className={cn("w-[70%] h-[70%] text-text-primary", iconClassName)}
-        fill="currentColor"
-      >
-        <path d="M 15 80 L 32 80 L 23.5 60 Z" />
-        <path d="M 33 55 L 45 30 L 65 30 C 78 30 85 38 85 50 C 85 58 78 65 68 68 L 78 80 L 60 80 L 52 68 L 47 68 L 52 80 L 35 80 L 45 55 Z M 48 55 L 55 55 C 65 55 68 50 68 45 C 68 40 65 38 55 38 L 41 38 Z" fillRule="evenodd" />
-      </svg>
+      <img
+        src={arkoreLogo}
+        alt="ARKORE LOGICS"
+        className={cn(
+          "w-auto h-auto max-w-[70%] max-h-[70%] object-contain",
+          iconClassName
+        )}
+      />
     </div>
   )
 }
